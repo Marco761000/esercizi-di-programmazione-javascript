bfa4b32ca9cfd105f67b57ed64513ec17c0e38bd
Marco761000 <marco.rocco76@libero.it>
2018-02-20 13:10:43 +0100

    Testing

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,16 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+
+var sec = 4022;
+var h = 3600;
+var m = 60;
+var s = 1;
+var ore = Math.round(sec/h);
+var hs = s*h;
+var secDopoOra = sec-hs;
+var secRimM = Math.round(secDopoOra/m);
+var minRimS = secRimM*m;
+var secRim = secDopoOra-minRimS;
+
+console.log(`${sec} secondi sono:${ore} ora, ${secRimM} minuti e ${secRim} secondi`);
