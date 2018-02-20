9070c5e73464c5af31aa0d16daba34b2d3509134
Marco761000 <marco.rocco76@libero.it>
2018-02-20 15:49:38 +0100

    Testing

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -7,15 +7,14 @@
   http://www.imparareaprogrammare.it
 */
 
-var sec = 4022;
+var sec = 12560;
 var h = 3600;
 var m = 60;
-var s = 1;
-var ore = Math.round(sec/h);
-var hs = s*h;
-var secDopoOra = sec-hs;
-var secRimM = Math.round(secDopoOra/m);
-var minRimS = secRimM*m;
-var secRim = secDopoOra-minRimS;
+var ore = Math.round(sec/h); //ore arrotondate
+var oreInS = h*ore;  //ore arrotondate trasformate in secondi
+var secRim = sec-oreInS; //secondi rimanenti dopo la sottrazione delle ore in secondi
+var min = Math.round(secRim/m); //minuti arrotondati
+var minInS = min*m;  //minuti arrotondati trasformati in secondi
+var secResto = secRim-minInS; //resto in secondi
 
-console.log(`${sec} secondi sono:${ore} ora, ${secRimM} minuti e ${secRim} secondi`);
+console.log(`${sec} secondi sono:${ore} ora, ${min} minuti e ${secResto} secondi`);
