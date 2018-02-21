daf37a29fe3c60cfdfde5eeb56c130edf573a005
Marco761000 <marco.rocco76@libero.it>
2018-02-21 10:17:36 +0100

    Testing

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -4,9 +4,9 @@
   http://www.imparareaprogrammare.it
 */
 var n = 15;
-var risultato = n%2;
-if (risultato > 0) {
-  console.log(1);
-}else {
-  console.log(0);
+var risultato = n%2; //assegno alla variabile n il modulo %2
+if (risultato > 0) { //se il risultato da resto, quindi il numero non è divisibile per 2, e quindi è maggiore di 0
+  console.log(1);    //allora restituisce 1
+}else {              //altrimenti
+  console.log(0);    //il numero non da resto, è divisibile per 2, è pari e restituisce 0
 }
