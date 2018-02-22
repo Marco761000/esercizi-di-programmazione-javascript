f46ca660c4a5138205fd200c6d81cd6eb2a06e44
Marco761000 <marco.rocco76@libero.it>
2018-02-22 10:33:28 +0100

    Testing

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -3,3 +3,11 @@
   Scrivi un programma che prenda quattro numeri in input, stampi in output il maggiore e il minore.
   http://www.imparareaprogrammare.it
 */
+var a = 45;
+var b = 2;
+var c = 81;
+var d = 74;
+var max = Math.max(a,b,c,d,);
+var min = Math.min(a,b,c,d,);
+console.log(`Il numero minore della lista è ${min}`);
+console.log(`Il numero maggiore della lista è ${max}`);
