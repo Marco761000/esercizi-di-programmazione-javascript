40347905cc7167fd5b8dc0fe4d08a98eaaa7afb2
Marco761000 <marco.rocco76@libero.it>
2018-02-23 10:09:54 +0100

    Testing2

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -3,55 +3,67 @@
   Scrivi un programma che prenda in input sette valori relativi alle temperature della settimana e stabilisca la giornata più calda e quella più fredda.
   http://www.imparareaprogrammare.it
 */
-var a = 12;
-var b = 4;
-var c = 13;
-var d = 5;
-var e = 10;
-var f = 8;
-var g = 2;
+var lunedì=3;
+var martedì=1;
+var mercoledì=5;
+var giovedì=8;
+var venerdì=2;
+var sabato=10;
+var domenica=7;
 
-if (a<b && a<c && a<d && a<e && a<f && a<g) {
-  console.log('Il giorno della settimana più freddo è Lunedì');
-}
-else if (b<a && b<c && b<d && b<e && b<f && b<g) {
-  console.log('Il giorno della settimana più freddo è Martedì');
-}
-else if (c<a && c<b && c<d && c<e && c<f && c<g) {
-  console.log('Il giorno della settimana più freddo è Mercoledì');
-}
-else if (d<a && d<b && d<c && d<e && d<f && d<g) {
-  console.log('Il giorno della settimana più freddo è Giovedì');
-}
-else if (e<a && e<b && e<c && e<d && e<f && e<g) {
-  console.log('Il giorno della settimana più freddo è Venerdì');
-}
-else if (f<a && f<b && f<c && f<d && f<e && f<g) {
-  console.log('Il giorno della settimana più freddo è Sabato');
-}
-else {
-  console.log('Il giorno della settimana più freddo è Domenica');
+var max=Math.max(lunedì,martedì,mercoledì,giovedì,venerdì,sabato,domenica);
+var min=Math.min(lunedì,martedì,mercoledì,giovedì,venerdì,sabato,domenica);
+
+var caldo='lunedì';
+var freddo='lunedì';
+
+switch(max) {
+case max=lunedì:
+caldo='lunedì';
+break;
+case max=martedì:
+caldo='martedì';
+break;
+case max=mercoledì:
+caldo='mercoledì';
+break;
+case max=giovedì:
+caldo='giovedì';
+break;
+case max=venerdì:
+hcaldo='venerdì';
+break;
+case max=sabato:
+caldo='sabato';
+break;
+case max=domenica:
+caldo='domenica';
+break;
 }
 
+switch(min) {
+case min=lunedì:
+freddo='lunedì';
+break;
+case min=martedì:
+freddo='martedì';
+break;
+case min=mercoledì:
+freddo='mercoledì';
+break;
+case min=giovedì:
+freddo='giovedì';
+break;
+case min=venerdì:
+freddo='venerdì';
+break;
+case min=sabato:
+freddo='sabato';
+break;
+case min=domenica:
+freddo='domenica';
+break;
+}
 
-if (a>b && a>c && a>d && a>e && a>f && a>g) {
-  console.log('Il giorno della settimana più caldo è Lunedì');
-}
-else if (b>a && b>c && b>d && b>e && b>f && b>g) {
-  console.log('Il giorno della settimana più caldo è Martedì');
-}
-else if (c>a && c>b && c>d && c>e && c>f && c>g) {
-  console.log('Il giorno della settimana più caldo è Mercoledì');
-}
-else if (d>a && d>b && d>c && d>e && d>f && d>g) {
-  console.log('Il giorno della settimana più caldo è Giovedì');
-}
-else if (e>a && e>b && e>c && e>d && e>f && e>g) {
-  console.log('Il giorno della settimana più caldo è Venerdì');
-}
-else if (f>a && f>b && f>c && f>d && f>e && f>g) {
-  console.log('Il giorno della settimana più caldo è Sabato');
-}
-else {
-  console.log('Il giorno della settimana più caldo è Domenica');
-}
+console.log('La giornata più calda è ' + caldo);
+console.log('La giornata più fredda è ' + freddo);
