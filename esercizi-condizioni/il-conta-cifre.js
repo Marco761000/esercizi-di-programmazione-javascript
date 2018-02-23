0cf0a4f643fdfa73029a2aa4e01bd54cdb1bf823
Marco761000 <marco.rocco76@libero.it>
2018-02-23 09:18:29 +0100

    Testing

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -3,3 +3,24 @@
   Scrivi un programma che calcoli quante cifre sono contenute in un numero inserito in input (massimo 9999).
   http://www.imparareaprogrammare.it
 */
+//Definisco una variabile n e la inizializzo.
+//Dividendo il numero per 10 se il risultato è minore di 1 allora il numero è formato da una cifra
+//altrimenti se dividendo il numero per 100, il risultato è minore di 1 allora il numero è formato da due cifre
+//altrimenti se dividendo il numero per 1000, il risultato è minore di 1 allora il numero è formato da tre cifre
+//altrimenti il numero è formato da quattro cifre
+
+
+var n = 123;
+
+if (n/10<1) {
+  console.log(`Il numero ${n} è formato da una cifra`);
+}
+else if (n/100<1) {
+  console.log(`Il numero ${n} è formato da due cifre`);
+}
+else if (n/1000<1) {
+  console.log(`Il numero ${n} è formato da tre cifre`);
+}
+else {
+  console.log(`Il numero ${n} è formato da quttro cifre`);
+}
