7966f0f5ff34f7522d40c82d0606e3ba03ee4ae1
Marco761000 <marco.rocco76@libero.it>
2018-02-27 10:41:54 +0100

    Testing

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -12,3 +12,33 @@
   Scriverne una versione che anziché i giorni della settimana, visualizzi i nomi dei mesi.
   http://www.imparareaprogrammare.it
 */
+console.log('GIORNI DELLA SETTIMANA \n1. Lunedì \n2. Martedì \n3. Mercoledì \n4. Giovedì \n5. Venerdì \n6. Sabato \n7. Domenica');
+
+var scelta = 5;
+
+switch (scelta) {
+  case 1:
+  console.log('Lunedì');
+  break;
+  case 2:
+  console.log('Martedì');
+  break;
+  case 3:
+  console.log('Mercoledì');
+  break;
+  case 4:
+  console.log('Giovedì');
+  break;
+  case 5:
+  console.log('Venerdì');
+  break;
+  case 6:
+  console.log('Sabato');
+  break;
+  case 7:
+  console.log('Domenica');
+  break;
+
+  default:
+  console.log('Mi dispiace, la tua scelta è errata');
+}
