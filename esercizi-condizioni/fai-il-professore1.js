b24be09f41df8a1b4d63bbb1258853c725345b09
Marco761000 <marco.rocco76@libero.it>
2018-02-27 11:33:33 +0100

    Testing-If

--- esercizi-condizioni/fai-il-professore1.js
+++ esercizi-condizioni/fai-il-professore1.js
@@ -13,3 +13,23 @@
   Realizzare una versione con il costrutto Switch-Case.
   http://www.imparareaprogrammare.it
 */
+var v = 24;
+
+if (v<18) {
+  console.log('insufficiente');
+}
+else if (v>=18 && v<21) {
+  console.log('sufficiente');
+}
+else if (v>=21 && v<24) {
+  console.log('buono');
+}
+else if (v>=24 && v<27) {
+  console.log('distinto');
+}
+else if (v>=27 && v<=29) {
+  console.log('ottimo');
+}
+else if (v == 30) {
+  console.log('eccellente');
+}
