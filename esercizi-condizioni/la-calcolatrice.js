bb1265e844abbc281065ee81e3d0c393585c6ecc
Marco761000 <marco.rocco76@libero.it>
2018-02-28 09:15:50 +0100

    Testing

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -14,3 +14,31 @@
   Utilizza il costrutto Switch-Case mediante acquisizione di caratteri.
   http://www.imparareaprogrammare.it
 */
+var a = 4;
+var b = 2;
+var operazione = 'moltiplicazione';
+var soluzione;
+
+switch (operazione) {
+  case 'somma':
+  soluzione = a+b;
+break;
+  case 'sottrazione':
+  soluzione = a-b;
+break;
+  case 'moltiplicazione':
+  soluzione = a*b;
+break;
+  case 'divisione':
+  soluzione = a/b;
+break;
+  case 'modulo':
+  soluzione = a%b;
+break;
+  case 'potenza':
+  soluzione = (a)**b;
+break;
+  case 'media':
+  soluzione = (a+b)/2;
+break;
+}
